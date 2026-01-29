7cda5e5b758e62da1069205d7e749c6a4375cc58
yasir <mohammadyasirkmt@gmail.com>
2026-01-29 22:06:53 +0530

    write plain PHP code in blade template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,11 @@
 	<h1>Welcome to Laravel</h1>
 	{{--<h3>Hey, {{ $name }}</h3>--}}
 	@include('dashboard', ['about' => 'Hey there I am learning PHP/Laravel'])
+
+	{{--@php
+		$msg = "I had write Plain PHP in blade template";
+		echo $msg;
+	@endphp--}}
+	{{--echo "Without @php tag in blade we can't write Plain PHP code" . PHP_EOL;--}}
 </body>
 </html>
